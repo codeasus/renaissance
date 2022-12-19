0935a030bf0f8a8ad5f33ca83fbfc00bdf114108
Orhan Rosenfeld <codeasus.orhan@outlook.com>
2022-12-20 01:18:53 +0400

    Initial commit

---
+++ app/src/main/java/codeasus/projects/renaissance/MainActivity.kt
@@ -0,0 +1,17 @@
+package codeasus.projects.renaissance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import codeasus.projects.renaissance.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+    }
+}
