4a9a4a64e1b90806c0c0798477728a31eb79e7f8
Orhan Rosenfeld <codeasus.orhan@outlook.com>
2023-01-31 19:58:38 +0400

    Commit last changes

--- app/src/main/java/codeasus/projects/renaissance/features/contanct/view/ContactFragment.kt
+++ app/src/main/java/codeasus/projects/renaissance/features/contanct/view/ContactFragment.kt
@@ -1,8 +1,13 @@
 package codeasus.projects.renaissance.features.contanct.view
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
@@ -17,6 +22,20 @@ class ContactFragment : Fragment() {
     private lateinit var mBinding: FragmentContactBinding
     private lateinit var mNavController: NavController
     private lateinit var mMenuHost: MenuHost
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
+            if (isGranted) {
+
+            } else {
+
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -53,4 +72,37 @@ class ContactFragment : Fragment() {
             }
         }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
+
+    private fun hasReadContactPermission(): Boolean {
+        return ActivityCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.READ_CONTACTS
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun hasWriteContactPermission(): Boolean {
+        return ActivityCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.WRITE_CONTACTS
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermissions() {
+        val permissionsToRequest = mutableListOf<String>()
+        if (hasReadContactPermission()) {
+            permissionsToRequest.add(Manifest.permission.READ_CONTACTS)
+        }
+        if (hasWriteContactPermission()) {
+            permissionsToRequest.add(Manifest.permission.WRITE_CONTACTS)
+        }
+
+        if (permissionsToRequest.isNotEmpty()) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                permissionsToRequest.toTypedArray(),
+                0
+            )
+        }
+    }
+    
 }
---
+++ app/src/main/java/codeasus/projects/util/ContactHelper.kt
@@ -0,0 +1,9 @@
+package codeasus.projects.util
+
+import android.content.Context
+
+object ContactHelper {
+    fun getAllContacts(context: Context) {
+
+    }
+}
